f977abaf18dd97df35d999b2ef800a751425872b
DvB97 <81575103+DvB97@users.noreply.github.com>
2021-05-04 12:35:16 -0500

    d
    das

--- Login/login.php
+++ Login/login.php
@@ -15,6 +15,6 @@
         header("Location: ../Cliente");
     }
     else{
-        header("Location: index.html?mensaje=Datos incorrectos");
+        header("Location: index.html?mensaje=Datos incorrectossss");
     } 
 ?>
